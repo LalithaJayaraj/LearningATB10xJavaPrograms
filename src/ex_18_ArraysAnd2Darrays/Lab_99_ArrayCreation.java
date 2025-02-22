d8f4631da28e9af11146ee8cf7389b19d3bce759
LalithaJayaraj <lalithartj@gmail.com>
2025-02-22 15:11:44 +0530

    Add files via upload

---
+++ src/ex_18_ArraysAnd2Darrays/Lab_99_ArrayCreation.java
@@ -0,0 +1,10 @@
+package ex_18_ArraysAnd2Darrays;
+
+public class Lab_99_ArrayCreation {
+    public static void main(String[] args) {
+        int[] nums = {10,15,20,9,70}; // This is integer array.
+        System.out.println(nums.length); // gives the length of the array.
+        String[] st = {"Hello","World","is","Beautiful"}; // string array is denoted by "".
+        System.out.println(st.length);// gives the length of the string array
+    }
+}
